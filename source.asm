f34418bc369ee856362c3134f8b50999721090af
Youssef60545 <youssefguzman78@gmail.com>
2025-03-14 21:36:18 -0600

    Update source.asm
    Documenté algunas lineas del codigo

--- source.asm
+++ source.asm
@@ -7,7 +7,7 @@
 	 JMP boot
 
 stackTop    EQU 0xFF    ; Inicializa SP
-txtDisplay  EQU 0x2E0
+txtDisplay  EQU 0x2E0	; Dirección de la salida de texto
 
 txtHola:    DB "Hola Mundo!"    ; Primera salida
             DB 0                ; Terminador de cadena
@@ -19,31 +19,31 @@ boot:
     MOV SP, stackTop    ; Configurar SP
     MOV C, txtHola      ; Apuntar C a "Hola Mundo!"
     MOV D, txtDisplay   ; Apuntar D a la salida
-    CALL print
+    CALL print		; llama a la rutina de impresión
 
     CALL delay          ; Esperar un momento
 
     MOV C, txtAdios     ; Apuntar C a "Adios Mundo!"
     MOV D, txtDisplay   ; Apuntar D a la salida
-    CALL print
+    CALL print		;llama a la rutina de impresión
 
     HLT                 ; Detener ejecución
 
 print:                  ; Imprimir cadena
-    PUSH A
-    PUSH B
-    MOV B, 0
+    PUSH A		;Guarda el valor de A en la pila
+    PUSH B		;Guarda el valor de B en la pila
+    MOV B, 0		;Iniializa B en 0 para comparación
 .loop:
     MOVB AL, [C]        ; Obtener carácter
     MOVB [D], AL        ; Escribir en la salida
-    INC C
-    INC D
+    INC C		;Mover al siguiente carácter en la cadena
+    INC D		;Mover a la siguiente posición en la salida
     CMPB BL, [C]        ; Verificar si es el terminador
     JNZ .loop           ; Repetir si no lo es
 
-    POP B
-    POP A
-    RET
+    POP B		;Restaurar el valor original de B desde la pila
+    POP A		;Restaurar el valor original de A desde la pila
+    RET			;Retornar desde la subrutina
 
 delay:                  ; Rutina de retardo
     PUSH A
