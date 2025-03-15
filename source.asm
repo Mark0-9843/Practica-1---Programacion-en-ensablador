83f6b5eea239a73e546f9c02ac10a6e3649812ad
Jesus Ibarra <ayudamedezmayo@gmail.com>
2025-03-14 22:10:11 -0600

    Update source.asm
    CODIGO

--- source.asm
+++ source.asm
@@ -1,55 +1,95 @@
 ; TEAM markoboquiñechainajesu
 
-; ESTE CÓDIGO REALIZA UNA IMPRESION DEL MENSAJE "HOLA MUNDO" Y LUEGO DE UNA PAUSA IMPRIME EL MENSAJE "ADIOS MUNDO"
+; ESTE CÓDIGO REALIZA UNA ANIMACION SIMPLE Y REALIZA UNA PREGUNTA
 
-; 14 / 03 / 2025 - V. 2. 0. 0 - PROGRAMACION EN ENSAMBLADOR
+; 14 / 03 / 2025 - V. 1. 0. 0 - PROGRAMACION EN ENSAMBLADOR
 
-	 JMP boot
+    JMP boot
 
 stackTop    EQU 0xFF    ; Inicializa SP
-txtDisplay  EQU 0x2E0	; Dirección de la salida de texto
+txtDisplay  EQU 0x2E0
 
-txtHola:    DB "Hola Mundo!"    ; Primera salida
-            DB 0                ; Terminador de cadena
+txtIntro:   DB "Esto es una animacion simple"  ; Mensaje inicial
+            DB 0
 
-txtAdios:   DB "Adios Mundo!"   ; Segunda salida
-            DB 0                ; Terminador de cadena
+txtClear:   DB "                                "  ; Mensaje para limpiar pantalla (32 espacios)
+            DB 0
+
+txtSmile:   DB ":)"  ; Primera fase de la animación
+            DB 0
+
+txtNeutral: DB ":|"  ; Segunda fase de la animación
+            DB 0
+
+txtSad:     DB ":("  ; Tercera fase de la animación
+            DB 0
+
+txtEnd:     DB "le gusto la animacion?"  ; Mensaje final
+            DB 0
 
 boot:
     MOV SP, stackTop    ; Configurar SP
-    MOV C, txtHola      ; Apuntar C a "Hola Mundo!"
+    MOV C, txtIntro     ; Apuntar C a "Esto es una animacion simple"
     MOV D, txtDisplay   ; Apuntar D a la salida
-    CALL print		; llama a la rutina de impresión
+    CALL print
 
-    CALL delay          ; Esperar un momento
+    CALL espera_larga     ; Esperar un poco
 
-    MOV C, txtAdios     ; Apuntar C a "Adios Mundo!"
+    MOV C, txtClear     ; Apuntar C a los espacios en blanco
     MOV D, txtDisplay   ; Apuntar D a la salida
-    CALL print		;llama a la rutina de impresión
+    CALL print          ; Limpiar pantalla
+
+    MOV C, txtSmile     ; Apuntar C a ":)"
+    MOV D, txtDisplay
+    CALL print
+    CALL espera_corta    ; Pequeña pausa
+
+    MOV C, txtNeutral   ; Apuntar C a ":|"
+    MOV D, txtDisplay
+    CALL print
+    CALL espera_corta    ; Pequeña pausa
+
+    MOV C, txtSad       ; Apuntar C a ":("
+    MOV D, txtDisplay
+    CALL print
+    CALL espera_larga     ; Pausa más larga
+
+    MOV C, txtEnd       ; Apuntar C a "le gusto la animacion?"
+    MOV D, txtDisplay
+    CALL print
 
     HLT                 ; Detener ejecución
 
 print:                  ; Imprimir cadena
-    PUSH A		;Guarda el valor de A en la pila
-    PUSH B		;Guarda el valor de B en la pila
-    MOV B, 0		;Iniializa B en 0 para comparación
+    PUSH A
+    PUSH B
+    MOV B, 0
 .loop:
     MOVB AL, [C]        ; Obtener carácter
     MOVB [D], AL        ; Escribir en la salida
-    INC C		;Mover al siguiente carácter en la cadena
-    INC D		;Mover a la siguiente posición en la salida
+    INC C
+    INC D
     CMPB BL, [C]        ; Verificar si es el terminador
     JNZ .loop           ; Repetir si no lo es
 
-    POP B		;Restaurar el valor original de B desde la pila
-    POP A		;Restaurar el valor original de A desde la pila
-    RET			;Retornar desde la subrutina
+    POP B
+    POP A
+    RET
 
-delay:                  ; Rutina de retardo
-    PUSH A		; Guardar el valor de A en la pila
-    MOV A, 0xFF		;Inicializar A con 0xFF
-.esperar:
-    DEC A		;Decrementar A
-    JNZ .wait		;Si A no es cero, continuar en el bucle
-    POP A		;Restarurar el valor original de A desde la pila
-    RET			;Retornar desde la subrutina
+espera_corta:            ; Rutina de espera corto
+    PUSH A
+    MOV A, 0x50
+    CALL esperar
+    RET
+
+espera_larga:             ; Rutina de espera larga
+    PUSH A
+    MOV A, 0xFF
+    CALL esperar
+    RET
+    
+esperar:
+    DEC A
+    JNZ esperar
+    POP A
+    RET
