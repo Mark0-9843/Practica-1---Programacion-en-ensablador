cb3f3d4947906a2d5676f6c0fa2659ac9c2a5f6b
Youssef60545 <youssefguzman78@gmail.com>
2025-03-14 22:30:07 -0600

    Update source.asm
    Documenté algunas lineas de código

--- source.asm
+++ source.asm
@@ -7,25 +7,25 @@
     JMP boot
 
 stackTop    EQU 0xFF    ; Inicializa SP
-txtDisplay  EQU 0x2E0
+txtDisplay  EQU 0x2E0    ;Dirección de la pantalla o buffer de salida
 
 txtIntro:   DB "Esto es una animacion simple"  ; Mensaje inicial
-            DB 0
+            DB 0    ;Terminador de cadena
 
 txtClear:   DB "                                "  ; Mensaje para limpiar pantalla (32 espacios)
-            DB 0
+            DB 0                                    ;Terminador de cadena
 
 txtSmile:   DB ":)"  ; Primera fase de la animación
-            DB 0
+            DB 0      ;Terminador de cadena
 
 txtNeutral: DB ":|"  ; Segunda fase de la animación
-            DB 0
+            DB 0        Terminador de cadena
 
 txtSad:     DB ":("  ; Tercera fase de la animación
-            DB 0
+            DB 0        ;Terminador de cadena
 
 txtEnd:     DB "le gusto la animacion?"  ; Mensaje final
-            DB 0
+            DB 0        ;Terminador de cadena
 
 boot:
     MOV SP, stackTop    ; Configurar SP
@@ -40,18 +40,18 @@ boot:
     CALL print          ; Limpiar pantalla
 
     MOV C, txtSmile     ; Apuntar C a ":)" Cargar la primera fase de la animación
-    MOV D, txtDisplay
-    CALL print
+    MOV D, txtDisplay    
+    CALL print            ;Imprimir la carita sonriente
     CALL espera_corta    ; Pequeña pausa
 
     MOV C, txtNeutral   ; Apuntar C a ":|" Cargar la segunda fase de la animación
     MOV D, txtDisplay
-    CALL print
+    CALL print            ;Imprimir la carita neutral
     CALL espera_corta    ; Pequeña pausa
 
     MOV C, txtSad       ; Apuntar C a ":(" Cargar la tercera fase de la animación
     MOV D, txtDisplay
-    CALL print
+    CALL print        ;Imprimir la carita triste
     CALL espera_larga     ; Pausa más larga
 
     MOV C, txtEnd       ; Apuntar C a "le gusto la animacion?"
